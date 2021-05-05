3ea0b17fe78a1788a843a27f9269dadac64ab427
rhino <alan.specchio@gmail.com>
2021-05-05 00:12:48 -0400

    updates

--- C/final.c
+++ C/final.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include <conio.h>
+#include <string.h>
 
 void blue() {
 	printf("\033[0;36m");
@@ -23,14 +25,29 @@ void colorReset() {
 	printf("\033[0m");
 }
 
+struct element {
+	char elementName[50];
+	char sb[5];
+	int atm;
+	float atms;
+	char block;
+	char atc[20];
+	char prop[250];
+};
+
+void examine();
+void addInfo();
+void menuSelection();
+char selection[2];
+
 int main() {
 	system("cls");
 	yellow();
 	printf("   Welcome...\n");
 	Sleep ( 300 );
 	printf("   Please enter your name: ");	
-	char name [20];
-	fscanf(stdin, "%20s", name);
+	char userName [20];
+	fscanf(stdin, "%20s", userName);
 	Sleep ( 1300 );
 	system("cls");
 	printf("\n\n\n");
@@ -60,15 +77,21 @@ int main() {
 	printf("\t       +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+\n");
 	colorReset();
 	printf("\n\n");
-	yellow();
-	fprintf(stdout,"\n   %s, ", name);
-    printf("Select an option, 1-3.  \n\n\n");
-	printf("\t\t 1) Examine\n");
-	printf("\t\t 2) Add information\n");
-	printf("\t\t 3) Exit\n\n");
-	int selection [1];
-	printf("     Selection: ");
-	fscanf(stdin, "%1d", selection);
-	colorReset();
 	return (420);
 }
+void menuSelection( char *userName) {	
+	yellow();
+	printf("\n   %s, ", userName);
+    printf("Select an option, 0-2.  \n\n\n");
+	printf("\t\t 0) Examine\n", menuSelection[0]);
+	printf("\t\t 1) Add information\n", menuSelection[1]);
+	printf("\t\t 2) Exit\n\n", menuSelection[2]);
+	int menuSelection [1];
+	printf("     Menu Selection: ");
+	fscanf(stdin, "%d", menuSelection);
+	colorReset();
+	
+}
+
+
+
